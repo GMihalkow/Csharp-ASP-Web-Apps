be0b851784997a363548739181096c592e4dc398
GMihalkow <32070470+GMihalkow@users.noreply.github.com>
2019-09-15 20:27:36 +0300

    Begun implementing the Orders administration page. Made the basic layout of the page using a JQuery IU accordion widget.

---
+++ ShopApp/ShopApp.Web/Models/Order/OrderViewModel.cs
@@ -0,0 +1,18 @@
+﻿using ShopApp.Models;
+using ShopApp.Web.Models;
+
+namespace ShopApp.Web
+{
+    public class OrderViewModel
+    {
+        public string Id { get; set; }
+        public ProductViewModel Product { get; set; }
+        public string UserId { get; set; }
+        public string User { get; set; }
+        public string Address { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public OrderStatus Status { get; set; }
+        public decimal TotalPrice => this.Quantity * this.Product.Price;
+    }
+}
