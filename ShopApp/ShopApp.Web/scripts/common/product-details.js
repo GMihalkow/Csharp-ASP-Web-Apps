0211dd1e941e5d35efd71e162e61e8ae39c0ece4
GMihalkow <32070470+GMihalkow@users.noreply.github.com>
2019-09-21 15:50:06 +0300

    Extracted the product details dialog JQuery UI functionality into a separate file.

---
+++ ShopApp/ShopApp.Web/scripts/common/product-details.js
@@ -0,0 +1,45 @@
+﻿product = {};
+product.dialog = {};
+
+product.dialog.configuration = {
+    autoOpen: false,
+    height: 620,
+    width: 500,
+    modal: true,
+    show: {
+        effect: "blind",
+        duration: 500
+    },
+    hide: {
+        effect: "explode",
+        duration: 500
+    },
+    // default buttons 
+    buttons: {
+        Cancel: function () {
+            productDetailsDialog.dialog("close");
+        }
+    }
+};
+// product details popup dialog
+var productDetailsDialog = $("#product-details-dialog");
+productDetailsDialog.dialog(product.dialog.configuration);
+
+var productDetailsButtons = $("[shop-action='product-details']");
+productDetailsButtons.on("click", function (e) {
+    e.preventDefault();
+
+    var productId = $(this).closest("[shop-product-id]").attr("shop-product-id");
+
+    $.ajax({
+        method: "GET",
+        url: "/Product/GetProduct/" + productId
+    }).then(function (product) {
+        var template = $("#product-details-template").html();
+        var rendered = Mustache.render(template, product);
+
+        $("#product-details-dialog").html(rendered);
+
+        productDetailsDialog.dialog("open");
+    });
+});
