75e59b122bb7faca65e4876b042176aa05515882
GMihalkow <32070470+GMihalkow@users.noreply.github.com>
2019-10-05 17:27:19 +0300

    Added the ProductAutocompleteViewModel.

---
+++ ShopApp/ShopApp.Web/Models/Product/ProductAutocompleteViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopApp.Web.Models.Product
+{
+    public class ProductAutocompleteViewModel
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
