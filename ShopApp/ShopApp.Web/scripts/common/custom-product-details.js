70f822b66b8c15e5c44356028e730c7ada8b775b
GMihalkow <32070470+GMihalkow@users.noreply.github.com>
2019-10-13 10:45:33 +0300

    Removed old product details modal partial and added a new one.

---
+++ ShopApp/ShopApp.Web/scripts/common/custom-product-details.js
@@ -0,0 +1,36 @@
+﻿function openProductDetailsDialog(productId, buttons) {
+    if (!buttons) {
+        buttons = [];
+    }
+
+    var modals = $(".modal-wrapper");
+    $.ajax({
+        method: "GET",
+        url: "/Product/GetProduct/" + productId
+    }).then(function (product) {
+        product.ModalHeading = "Product Details";
+
+        var template = $("#product-details-template").html();
+        var rendered = Mustache.render(template, product);
+
+        $(".custom-modal").html(rendered);
+
+        modals.fadeIn(200);
+        modals.find(".custom-modal").fadeIn(500);
+        var modalFooter = $(".custom-modal").find(".modal-footer");
+        Array.from(buttons).forEach(function (btn) {
+            var renderedButton = $("<button class='modal-btn'>" + btn.Name + "</button>");
+            $(renderedButton).on("click", btn.onClick);
+            modalFooter.append(renderedButton);
+        });
+
+        var closeModalBtns = $(".custom-modal").find("[close-modal-btn]");
+        closeModalBtns.on("click", function (e) {
+            var currentTarget = $(e.currentTarget);
+            var currentModal = currentTarget.closest(".modal-wrapper");
+
+            currentModal.fadeOut(200);
+            currentModal.find(".custom-modal").fadeOut(500);
+        });
+    });
+}
