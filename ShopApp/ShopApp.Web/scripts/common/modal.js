3dba75fb353be1c3ee24749f9c7f9ec954385131
GMihalkow <32070470+GMihalkow@users.noreply.github.com>
2019-10-13 10:43:26 +0300

    Added a custom made Modal. With a partial.

---
+++ ShopApp/ShopApp.Web/scripts/common/modal.js
@@ -0,0 +1,16 @@
+﻿$(function () {
+    var modals = $(".modal-wrapper");
+
+    var openModalBtns = $("[modal-btn]");
+    openModalBtns.on("click", function () {
+        modals.fadeIn(200);
+        modals.find(".custom-modal").fadeIn(500);
+    });
+
+    modals.on("click", function (e) {
+        if ($(e.target).attr("class") === "modal-wrapper") {
+            modals.fadeOut(200);
+            modals.find(".custom-modal").fadeOut(500);
+        }
+    });
+});
