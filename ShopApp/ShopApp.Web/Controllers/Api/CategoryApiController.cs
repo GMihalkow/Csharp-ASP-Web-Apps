4d4e9c090a0112824c22b2e85f020dbf97c5cd99
GMihalkow <32070470+GMihalkow@users.noreply.github.com>
2019-10-20 15:54:38 +0300

    Extracted needed methods from the CategoryController to the CategoryApiController.

---
+++ ShopApp/ShopApp.Web/Controllers/Api/CategoryApiController.cs
@@ -0,0 +1,42 @@
+﻿using ShopApp.Web.Models;
+using ShopApp.Web.Services.Category.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Web.Http.Results;
+using System.Web.Mvc;
+
+namespace ShopApp.Web.Controllers.Api
+{
+    public class CategoryApiController : BaseApiController
+    {
+        private readonly ICategoryService categoryService;
+
+        public CategoryApiController(ICategoryService categoryService)
+        {
+            this.categoryService = categoryService;
+        }
+
+        [Authorize]
+        public JsonResult<CategoryViewModel> Get(string id)
+        {
+            CategoryViewModel categoryModel = this.categoryService.GetCategory(id);
+
+            return this.Json(categoryModel);
+        }
+
+        // TODO [GM]: Is this even used anywhere?
+        [HttpGet]
+        [Route(template: "/api/GetCategoryProducts/{id}")]
+        public JsonResult<IEnumerable<ProductViewModel>> GetCategoryProducts(string id)
+        {
+            CategoryViewModel category = this.categoryService.GetCategory(id);
+
+            if (category == null)
+            {
+                throw new InvalidOperationException("Invalid Category id.");
+            }
+
+            return this.Json(category.Products);
+        }
+    }
+}
--- ShopApp/ShopApp.Web/Controllers/Category/CategoryController.cs
+++ ShopApp/ShopApp.Web/Controllers/Category/CategoryController.cs
@@ -45,32 +45,12 @@ namespace ShopApp.Web.Controllers.Category
             return this.RedirectToAction("All", "Product");
         }
 
-        public ActionResult GetCategoryProducts(string id)
-        {
-            CategoryViewModel category = this.categoryService.GetCategory(id);
-
-            if (category == null)
-            {
-                throw new InvalidOperationException("Invalid Category id.");
-            }
-
-            return this.Json(category.Products, JsonRequestBehavior.AllowGet);
-        }
-
         [Authorize(Roles = RolesConstants.Administrator)]
         public async Task<ActionResult> Delete(string id)
         {
             await this.categoryService.Delete(id);
 
             return this.RedirectToAction("All", "Product");
-        }
-
-        [Authorize]
-        public ActionResult Get(string id)
-        {
-            CategoryViewModel categoryModel = this.categoryService.GetCategory(id);
-
-            return this.Json(categoryModel, JsonRequestBehavior.AllowGet);
         }
     }
 }
