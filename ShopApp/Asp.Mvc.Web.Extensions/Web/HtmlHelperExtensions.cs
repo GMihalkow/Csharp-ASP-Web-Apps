f46d2053125642b8d3b0b63650e4c25772f4437b
GMihalkow <32070470+GMihalkow@users.noreply.github.com>
2019-10-23 22:08:36 +0300

    Added an Extensions project for the Web part of the project.

---
+++ ShopApp/Asp.Mvc.Web.Extensions/Web/HtmlHelperExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Asp.Mvc.Web.Extensions.Web
+{
+    public static class HtmlHelperExtensions
+    {
+        public static string CurrentController(this HtmlHelper htmlHelper)
+        {
+            return htmlHelper.ViewContext.RouteData.Values["controller"].ToString();
+        }
+    }
+}
