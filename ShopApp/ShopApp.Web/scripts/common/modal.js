c99c9c0156e6a9f94d16ba18c2b6cdda47139158
GMihalkow <32070470+GMihalkow@users.noreply.github.com>
2019-10-27 15:14:11 +0200

    Extracted all the modal methods to one JavaScript file modal.js and extracted all the modal functionality to one object. Replaced it everywhere where it is used.

--- ShopApp/ShopApp.Web/scripts/common/modal.js
+++ ShopApp/ShopApp.Web/scripts/common/modal.js
@@ -1,71 +1,95 @@
 ﻿$(function () {
     var modals = $(".modal-wrapper");
 
-    //var openModalBtns = $("[modal-btn]");
-    //openModalBtns.on("click", function () {
-    //    modals.fadeIn(200);
-    //    modals.find(".custom-modal").fadeIn(500);
-    //});
-
     modals.on("click", function (e) {
         if ($(e.target).attr("class") === "modal-wrapper") {
             modals.fadeOut(200);
             modals.find(".custom-modal").fadeOut(500);
         }
     });
+
 });
 
-function openModal(heading, body, footerBtns) {
-    if (!footerBtns) {
-        footerBtns = [];
-    }
+var _this = this;
 
-    var closeModalBtnExists = footerBtns.some(function (btn) {
-        return btn.Name === "Cancel";
-    });
+var modalFunctions = {
+    openModal: function openModal(heading, body, footerBtns) {
+        if (!footerBtns) {
+            footerBtns = [];
+        }
 
-    if (!closeModalBtnExists) {
-        var closeModalBtn = {
-            Name: "Cancel",
-            onClick: function () {
-                $(".custom-modal").find("[close-modal-btn]").click();
+        var closeModalBtnExists = footerBtns.some(function (btn) {
+            return btn.Name === "Cancel";
+        });
+
+        if (!closeModalBtnExists) {
+            var closeModalBtn = {
+                Name: "Cancel",
+                onClick: function () {
+                    $(".custom-modal").find("[close-modal-btn]").click();
+                }
+            };
+
+            footerBtns.push(closeModalBtn);
+        }
+
+        var modals = $(".modal-wrapper");
+
+        modals.fadeIn(200);
+        modals.find(".custom-modal").fadeIn(500);
+
+        modals.find(".modal-header").find("h3").text(heading);
+
+        var modalBody = modals.find(".modal-body");
+        modalBody.html("");
+        modalBody.html(body);
+
+        var $form = modalBody.find(".modal-form");
+        if ($form.length > 0) {
+            $.validator.unobtrusive.parse($form);
+        }
+
+        var modalFooter = $(".custom-modal").find(".modal-footer");
+        modalFooter.html("");
+        Array.from(footerBtns).forEach(function (btn) {
+            var renderedButton = $("<button class='modal-btn'>" + btn.Name + "</button>");
+            $(renderedButton).on("click", btn.onClick);
+            modalFooter.append(renderedButton);
+        });
+
+        // TODO [GM]: Don't attach event every time?
+        var closeModalBtns = $(".custom-modal").find("[close-modal-btn]");
+        closeModalBtns.on("click", function (e) {
+            var currentTarget = $(e.currentTarget);
+            var currentModal = currentTarget.closest(".modal-wrapper");
+
+            currentModal.fadeOut(200);
+            currentModal.find(".custom-modal").fadeOut(500);
+        });
+    },
+
+    openConfirmationModal: function openConfirmationModal(confirmCallback) {
+        var body = "<p>Are you sure that u want to continue?</p>";
+
+        var buttons = [
+            {
+                Name: "Confirm",
+                onClick: confirmCallback
             }
-        };
+        ];
 
-        footerBtns.push(closeModalBtn);
+        _this.modalFunctions.openModal("Confirm", body, buttons);
+    },
+
+    openProductDetailsDialog: function openProductDetailsDialog(productId, buttons) {
+        $.ajax({
+            method: "GET",
+            url: "/api/ProductApi/Get/" + productId
+        }).then(function (product) {
+            var template = $("#product-details-template").html();
+            var rendered = Mustache.render(template, product);
+
+            _this.modalFunctions.openModal("Product Details", rendered, buttons);
+        });
     }
-
-    var modals = $(".modal-wrapper");
-
-    modals.fadeIn(200);
-    modals.find(".custom-modal").fadeIn(500);
-
-    modals.find(".modal-header").find("h3").text(heading);
-
-    var modalBody = modals.find(".modal-body");
-    modalBody.html("");
-    modalBody.html(body);
-
-    var $form = modalBody.find(".modal-form");
-    if ($form.length > 0) {
-        $.validator.unobtrusive.parse($form);
-    }
-
-    var modalFooter = $(".custom-modal").find(".modal-footer");
-    modalFooter.html("");
-    Array.from(footerBtns).forEach(function (btn) {
-        var renderedButton = $("<button class='modal-btn'>" + btn.Name + "</button>");
-        $(renderedButton).on("click", btn.onClick);
-        modalFooter.append(renderedButton);
-    });
-
-    // TODO [GM]: Don't attach event every time?
-    var closeModalBtns = $(".custom-modal").find("[close-modal-btn]");
-    closeModalBtns.on("click", function (e) {
-        var currentTarget = $(e.currentTarget);
-        var currentModal = currentTarget.closest(".modal-wrapper");
-
-        currentModal.fadeOut(200);
-        currentModal.find(".custom-modal").fadeOut(500);
-    });
-}
+};
