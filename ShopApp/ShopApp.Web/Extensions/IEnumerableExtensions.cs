27fe06580b02555c4fe51cb1a1a6bb9d13163d08
GMihalkow <32070470+GMihalkow@users.noreply.github.com>
2019-10-27 15:21:58 +0200

    Added extensions for IEnumerable.

---
+++ ShopApp/ShopApp.Web/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace ShopApp.Web
+{
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<SelectListItem> ToSelectList<T>(this IEnumerable<T> collection, Func<T, object> textFunc, Func<T, object> valueFunc) where T : class
+        {
+            return collection.Select(e => new SelectListItem { Text = textFunc(e).ToString(), Value = valueFunc.ToString() });
+        }
+    }
+}
