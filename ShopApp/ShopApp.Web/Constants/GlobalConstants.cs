0ef9cf76f5d561b8ab5450609373923772001e59
GMihalkow <32070470+GMihalkow@users.noreply.github.com>
2019-10-27 15:19:29 +0200

    Added a URL regular expression in the GlobalConstants and started using it in the ProductInputModel.

--- ShopApp/ShopApp.Web/Constants/GlobalConstants.cs
+++ ShopApp/ShopApp.Web/Constants/GlobalConstants.cs
@@ -3,5 +3,7 @@
     public class GlobalConstants
     {
         public const int PageSize = 9;
+
+        public const string UrlRegex = @"^(https|http)?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/\/=]*)$";
     }
 }
--- ShopApp/ShopApp.Web/Models/Product/ProductInputModel.cs
+++ ShopApp/ShopApp.Web/Models/Product/ProductInputModel.cs
@@ -2,17 +2,23 @@
 
 namespace ShopApp.Web.Models
 {
+    // TODO [GM]: Use a ProductBaseInputModel like at work?
     public class ProductInputModel
     {
-        public string Id { get; set; }
         [Required]
         [MinLength(3)]
         public string Name { get; set; }
+
         public string Description { get; set; }
+
         [Required]
         public decimal Price { get; set; }
+
+        [Required]
+        [RegularExpression(pattern: GlobalConstants.UrlRegex, ErrorMessage = "You must provide a valid URL via HTTP or HTTPS.")]
         [DataType(DataType.ImageUrl)]
         public string CoverUrl { get; set; }
+
         public string CategoryId { get; set; }
     }
 }
