eb69ccf27d73bfcad35b232043c94c4ecd7a7da1
GMihalkow <32070470+GMihalkow@users.noreply.github.com>
2019-11-10 11:49:27 +0200

    Added a LoggedUserId property to the BaseController.

--- ShopApp/ShopApp.Web/Controllers/BaseController.cs
+++ ShopApp/ShopApp.Web/Controllers/BaseController.cs
@@ -1,8 +1,26 @@
-﻿using System.Web.Mvc;
+﻿using Microsoft.AspNet.Identity.Owin;
+using ShopApp.Web.Utilities;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 
 namespace ShopApp.Web.Controllers
 {
     public abstract class BaseController : Controller
     {
+        public string LoggedUserId
+        {
+            get
+            {
+                if (this.User.Identity.IsAuthenticated)
+                {
+                    var userManager = this.HttpContext.GetOwinContext().GetUserManager<ShopUserManager>();
+
+                    return userManager.Users.FirstOrDefault(u => u.UserName == this.User.Identity.Name).Id;
+                }
+
+                return null;
+            }
+        }
     }
 }
