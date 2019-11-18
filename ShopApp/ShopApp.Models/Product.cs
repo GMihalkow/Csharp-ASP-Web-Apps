3588eddedcb0e702dd786a2ba65e12b8f87d031c
GMihalkow <32070470+GMihalkow@users.noreply.github.com>
2019-11-18 22:28:20 +0200

    Added a StockCount column to the products table and respectively a new StockCount property to the Product object.

--- ShopApp/ShopApp.Models/Product.cs
+++ ShopApp/ShopApp.Models/Product.cs
@@ -7,6 +7,7 @@ namespace ShopApp.Models
 	{
 		public string Name { get; set; }
 		public decimal Price { get; set; }
+        public int StockCount { get; set; }
         public string Description { get; set; }
         public string CoverUrl { get; set; }
 		public string CategoryId { get; set; }
