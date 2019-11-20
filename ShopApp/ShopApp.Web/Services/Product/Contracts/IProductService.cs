049e26681c9570976cdbeb92b07c6ee569fc5cf8
GMihalkow <32070470+GMihalkow@users.noreply.github.com>
2019-11-20 23:14:57 +0200

    Added Exists & DecrementProductStockCount methods to the ProductService in order to implement the stock count functionality for the products.

--- ShopApp/ShopApp.Web/Services/Product/Contracts/IProductService.cs
+++ ShopApp/ShopApp.Web/Services/Product/Contracts/IProductService.cs
@@ -1,7 +1,13 @@
-﻿namespace ShopApp.Web.Services.Product.Contracts
+﻿using System.Threading.Tasks;
+
+namespace ShopApp.Web.Services.Product.Contracts
 {
     public interface IProductService
     {
         int ProductsCountByCategory(string category);
+
+        bool Exists(string id);
+
+        Task DecrementProductStockCount(string id, int quantity);
     }
 }
--- ShopApp/ShopApp.Web/Services/Product/ProductService.cs
+++ ShopApp/ShopApp.Web/Services/Product/ProductService.cs
@@ -1,7 +1,11 @@
 ﻿using ShopApp.Data;
+using ShopApp.Web.Models;
+using ShopApp.Web.Repositories.Contracts;
 using ShopApp.Web.Services.Category.Contracts;
 using ShopApp.Web.Services.Product.Contracts;
+using System;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace ShopApp.Web.Services.Product
 {
@@ -9,11 +13,36 @@ namespace ShopApp.Web.Services.Product
     {
         private readonly ICategoryService categoryService;
         private readonly ShopAppDbContext dbContext;
+        private readonly IRepository<ProductViewModel, ProductBaseInputModel> productRepository;
 
-        public ProductService(ICategoryService categoryService, ShopAppDbContext dbContext)
+        public ProductService(ICategoryService categoryService, ShopAppDbContext dbContext, IRepository<ProductViewModel, ProductBaseInputModel> productRepository)
         {
             this.categoryService = categoryService;
             this.dbContext = dbContext;
+            this.productRepository = productRepository;
+        }
+
+        public bool Exists(string id) => this.dbContext.Products.Any(product => product.Id == id);
+
+        public async Task DecrementProductStockCount(string id, int quantity)
+        {
+            bool productExists = this.Exists(id);
+            if (productExists)
+            {
+                ShopApp.Models.Product product = this.dbContext.Products.FirstOrDefault(p => p.Id == id);
+
+                if (product.StockCount - quantity < 0)
+                {
+                    throw new InvalidOperationException($"Not enough quantity of {product.Name} product.");
+                }
+
+                product.StockCount = product.StockCount - quantity;
+                await this.dbContext.SaveChangesAsync();
+            }
+            else
+            {
+                throw new InvalidOperationException($"Invalid product id.");
+            }
         }
 
         public int ProductsCountByCategory(string category)
