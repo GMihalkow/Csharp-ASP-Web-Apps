62f0b45d12d386a90263e69cb892e32113fb4f63
GMihalkow <32070470+GMihalkow@users.noreply.github.com>
2019-11-25 00:49:32 +0200

    Added IQueryableExtensions with one extension method for now.

---
+++ ShopApp/ShopApp.Web/Extensions/IQueryableExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ShopApp.Web
+{
+    public static class IQueryableExtensions
+    {
+        public static IQueryable<T> Include<T>(this IQueryable<T> source, Expression<Func<T, object>> propertyExpression)
+        {
+            var member = propertyExpression.Body as MemberExpression;
+            if (member == null){ throw new ArgumentException("Expression doesn't refer to a property."); }
+
+            return source.Include(member.Member.Name);
+        }
+    }
+}
