6b4f1fef74665e9c83347afbe4cb204f6e4d69ba
GMihalkow <32070470+GMihalkow@users.noreply.github.com>
2019-11-27 22:02:57 +0200

    Added a new extension in HtmlHelperExtensions, ActionLinkWithoutArea.

--- ShopApp/Asp.Mvc.Web.Extensions/Web/HtmlHelperExtensions.cs
+++ ShopApp/Asp.Mvc.Web.Extensions/Web/HtmlHelperExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Mvc;
+using System.Web.Mvc.Html;
 
 namespace Asp.Mvc.Web.Extensions.Web
 {
@@ -7,6 +8,11 @@ namespace Asp.Mvc.Web.Extensions.Web
         public static string CurrentController(this HtmlHelper htmlHelper)
         {
             return htmlHelper.ViewContext.RouteData.Values["controller"].ToString();
+        }
+
+        public static MvcHtmlString ActionLinkWithoutArea(this HtmlHelper htmlHelper, string linkText, string actionName, string controllerName, object htmlAttributes = default(object))
+        {
+            return htmlHelper.ActionLink(linkText, actionName, controllerName, new { area = string.Empty }, htmlAttributes);
         }
     }
 }
