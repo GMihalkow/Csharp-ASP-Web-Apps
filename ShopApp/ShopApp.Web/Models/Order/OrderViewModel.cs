87053ae3530c577263a8adb46ea3ae6b6655c789
GMihalkow <32070470+GMihalkow@users.noreply.github.com>
2019-12-01 15:51:04 +0200

    Code clean up.

--- ShopApp/ShopApp.Web/Models/Order/OrderViewModel.cs
+++ ShopApp/ShopApp.Web/Models/Order/OrderViewModel.cs
@@ -4,17 +4,27 @@ using System;
 
 namespace ShopApp.Web
 {
+    // TODO [GM]: Move to Areas/Admin/Models?
     public class OrderViewModel
     {
         public string Id { get; set; }
+
         public ProductViewModel Product { get; set; }
+
         public string UserId { get; set; }
+
         public string User { get; set; }
+
         public string Address { get; set; }
+
         public string Description { get; set; }
+
         public DateTime OrderedOn { get; set; }
+
         public int Quantity { get; set; }
+
         public OrderStatus Status { get; set; }
+
         public decimal TotalPrice => this.Quantity * this.Product.Price;
     }
 }
--- ShopApp/ShopApp.Web/Services/Order/Contracts/IOrderService.cs
+++ ShopApp/ShopApp.Web/Services/Order/Contracts/IOrderService.cs
@@ -3,12 +3,13 @@ using System.Threading.Tasks;
 
 namespace ShopApp.Web.Services.Order.Contracts
 {
+    // TODO [GM]: Test concurrent orders for users.
     public interface IOrderService
     {
         Task<ShopApp.Models.Order> Get(string id);
 
         Task CancelOrder(string orderId);
-
+        
         Task<string> Checkout(string ordersJson);
 
         Task SendOrder(string orderId);
