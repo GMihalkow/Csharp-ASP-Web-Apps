7f17a394c312c3e46b012fb64e73e08593d9d0f9
GMihalkow <32070470+GMihalkow@users.noreply.github.com>
2019-12-01 15:48:37 +0200

    Added an edit product StockCount administration page in the Admin area. with a model.

--- ShopApp/ShopApp.Web/Areas/Admin/Controllers/AdministrationController.cs
+++ ShopApp/ShopApp.Web/Areas/Admin/Controllers/AdministrationController.cs
@@ -1,8 +1,11 @@
-﻿using ShopApp.Web.Constants;
+﻿using Newtonsoft.Json;
+using ShopApp.Web.Constants;
 using ShopApp.Web.Controllers;
 using ShopApp.Web.Services.Order.Contracts;
+using ShopApp.Web.Services.Product.Contracts;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 
@@ -12,13 +15,15 @@ namespace ShopApp.Web.Areas.Admin.Controllers
     public class AdministrationController : BaseController
     {
         public readonly IOrderService orderService;
+        private readonly IProductService productService;
 
-        public AdministrationController(IOrderService orderService)
+        public AdministrationController(IOrderService orderService, IProductService productService)
         {
             this.orderService = orderService;
+            this.productService = productService;
         }
 
-        // TODO [GM]: Make async?
+        // TODO [GM]: Make async? 
         public ActionResult Orders()
         {
             List<OrderViewModel> orders = this.orderService.GetOrders().ToList();
@@ -29,5 +34,11 @@ namespace ShopApp.Web.Areas.Admin.Controllers
         public async Task Send(string id) => await this.orderService.SendOrder(id);
 
         public async Task Complete(string id) => await this.orderService.CompleteOrder(id);
+
+        // TODO [GM]: Implement the add stock count functionality
+        public ActionResult Products()
+        {
+            return this.View();
+        }
     }
 }
