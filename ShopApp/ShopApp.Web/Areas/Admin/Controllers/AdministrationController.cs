3a19deaea006c2c31449c6d569c26e84b51378e1
Georgimihalkov <Georgimihalkov@abv.bg>
2019-12-10 18:47:38 +0200

    Extracted Admin/Administration/Orders logic to the OrderApiController. The View now fetches the orders from there and uses them as json.

--- ShopApp/ShopApp.Web/Areas/Admin/Controllers/AdministrationController.cs
+++ ShopApp/ShopApp.Web/Areas/Admin/Controllers/AdministrationController.cs
@@ -1,11 +1,6 @@
-﻿using Newtonsoft.Json;
-using ShopApp.Web.Constants;
+﻿using ShopApp.Web.Constants;
 using ShopApp.Web.Controllers;
 using ShopApp.Web.Services.Order.Contracts;
-using ShopApp.Web.Services.Product.Contracts;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 
@@ -15,30 +10,18 @@ namespace ShopApp.Web.Areas.Admin.Controllers
     public class AdministrationController : BaseController
     {
         public readonly IOrderService orderService;
-        private readonly IProductService productService;
 
-        public AdministrationController(IOrderService orderService, IProductService productService)
+        public AdministrationController(IOrderService orderService)
         {
             this.orderService = orderService;
-            this.productService = productService;
         }
 
-        // TODO [GM]: Make async? 
-        public ActionResult Orders()
-        {
-            List<OrderViewModel> orders = this.orderService.GetOrders().ToList();
-
-            return this.View(orders);
-        }
+        public ActionResult Orders() => this.View();
 
         public async Task Send(string id) => await this.orderService.SendOrder(id);
 
         public async Task Complete(string id) => await this.orderService.CompleteOrder(id);
 
-        // TODO [GM]: Implement the add stock count functionality
-        public ActionResult Products()
-        {
-            return this.View();
-        }
+        public ActionResult Products() => this.View();
     }
 }
---
+++ ShopApp/ShopApp.Web/Controllers/Api/OrderApiController.cs
@@ -0,0 +1,28 @@
+﻿using ShopApp.Web.Constants;
+using ShopApp.Web.Services.Order.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace ShopApp.Web.Controllers.Api
+{
+    public class OrderApiController : BaseApiController
+    {
+        private readonly IOrderService orderService;
+
+        public OrderApiController(IOrderService orderService)
+        {
+            this.orderService = orderService;
+        }
+
+        [Authorize(Roles = RolesConstants.Administrator)]
+        [Route(template: "api/OrderApi/GetOrders")]
+        public JsonResult<List<OrderViewModel>> GetOrders()
+        {
+            List<OrderViewModel> orders = this.orderService.GetOrders().ToList();
+
+            return this.Json(orders);
+        }
+    }
+}
