e033492ab638a80e3053bd73bdcbf25821f1ebd5
Georgimihalkov <Georgimihalkov@abv.bg>
2019-12-16 22:09:49 +0200

    Moved the IEnumerableExtensions & IQueryableExtensions to the Dal project.

--- ShopApp/ShopApp.Dal/Extensions/IEnumerableExtensions.cs
+++ ShopApp/ShopApp.Dal/Extensions/IEnumerableExtensions.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web.Mvc;
 
-namespace ShopApp.Web
+namespace ShopApp.Dal
 {
     public static class IEnumerableExtensions
     {
-        public static IEnumerable<SelectListItem> ToSelectList<T>(this IEnumerable<T> collection, Func<T, object> textFunc, Func<T, object> valueFunc) where T : class
-        {
-            return collection.Select(e => new SelectListItem { Text = textFunc(e).ToString(), Value = valueFunc.ToString() });
-        }
 
         public static bool IsNullOrEmpty<T>(this IEnumerable<T> collection)
         {
