a608030450457f453b4673037b016e360de03010
Georgimihalkov <Georgimihalkov@abv.bg>
2019-12-21 10:47:34 +0200

    Created an Api project in which we will extract all the web api functionality from the main web app project.

---
+++ ShopApp/ShopApp.Api/Global.asax.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Http;
+using System.Web.Mvc;
+using System.Web;
+
+namespace ShopApp.Api
+{
+    public class WebApiApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+        }
+    }
+}
