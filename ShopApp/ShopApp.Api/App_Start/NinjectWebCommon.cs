f8772d2b52b601da6fe8a24430968e55edcff1b5
GMihalkow <32070470+GMihalkow@users.noreply.github.com>
2019-12-23 17:05:14 +0200

    Added the needed packages for the web api project. And converted it back to the package.config style of dealing with nuget packages. Added Ninject as a dependency resolver.

---
+++ ShopApp/ShopApp.Api/App_Start/NinjectWebCommon.cs
@@ -0,0 +1,93 @@
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(ShopApp.Api.App_Start.NinjectWebCommon), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethod(typeof(ShopApp.Api.App_Start.NinjectWebCommon), "Stop")]
+
+namespace ShopApp.Api.App_Start
+{
+    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+    using Ninject;
+    using Ninject.Web.Common;
+    using Ninject.Web.Common.WebHost;
+    using Ninject.Web.WebApi;
+    using ShopApp.Dal;
+    using ShopApp.Dal.Repositories;
+    using ShopApp.Dal.Repositories.Contracts;
+    using ShopApp.Dal.Services.Category;
+    using ShopApp.Dal.Services.Category.Contracts;
+    using ShopApp.Dal.Services.Order;
+    using ShopApp.Dal.Services.Order.Contracts;
+    using ShopApp.Dal.Services.Product;
+    using ShopApp.Dal.Services.Product.Contracts;
+    using ShopApp.Dal.Services.User;
+    using ShopApp.Dal.Services.User.Contracts;
+    using System;
+    using System.Web;
+    using System.Web.Http;
+
+    public static class NinjectWebCommon
+    {
+        private static readonly Bootstrapper bootstrapper = new Bootstrapper();
+
+        /// <summary>
+        /// Starts the application
+        /// </summary>
+        public static void Start()
+        {
+            DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
+            DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
+            bootstrapper.Initialize(CreateKernel);
+        }
+
+        /// <summary>
+        /// Stops the application.
+        /// </summary>
+        public static void Stop()
+        {
+            bootstrapper.ShutDown();
+        }
+
+        /// <summary>
+        /// Creates the kernel that will manage your application.
+        /// </summary>
+        /// <returns>The created kernel.</returns>
+        private static IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            try
+            {
+                kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
+                kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
+
+                RegisterServices(kernel);
+
+                GlobalConfiguration.Configuration.DependencyResolver = new NinjectDependencyResolver(kernel);
+
+                return kernel;
+            }
+            catch
+            {
+                kernel.Dispose();
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Load your modules or register your services here!
+        /// </summary>
+        /// <param name="kernel">The kernel.</param>
+        private static void RegisterServices(IKernel kernel)
+        {
+            // injecting the repositories
+            kernel.Bind<IRepository<CategoryViewModel, CategoryInputModel>>().To<CategoryRepository>().InRequestScope();
+            kernel.Bind<IRepository<ProductViewModel, ProductBaseInputModel>>().To<ProductRepository>().InRequestScope();
+
+            // injecting the db context
+            //kernel.Bind<ShopAppDbContext>().To<ShopAppDbContext>().InRequestScope();
+
+            // injecting the services
+            kernel.Bind<IUserService>().To<UserService>().InRequestScope();
+            kernel.Bind<ICategoryService>().To<CategoryService>().InRequestScope();
+            kernel.Bind<IProductService>().To<ProductService>().InRequestScope();
+            kernel.Bind<IOrderService>().To<OrderService>().InRequestScope();
+        }
+    }
+}
