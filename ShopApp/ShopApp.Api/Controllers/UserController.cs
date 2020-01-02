f497ddd03648460c01917c1ccffc76fd67adbce9
Georgimihalkov <Georgimihalkov@abv.bg>
2020-01-02 15:19:39 +0000

    Added UserController to the API project.

---
+++ ShopApp/ShopApp.Api/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using ShopApp.Dal.Models.User;
+using ShopApp.Dal.Services.User.Contracts;
+using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace ShopApp.Api.Controllers
+{
+    public class UserController : BaseApiController
+    {
+        private readonly IUserService userService;
+
+        public UserController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        [HttpGet]
+        [Route(template: "api/User/All")]
+        public JsonResult<IEnumerable<UserViewModel>> All()
+        {
+             var users = this.userService.GetAllUsersViewModels();
+            
+            return this.Json(users);
+        }
+    }
+}
