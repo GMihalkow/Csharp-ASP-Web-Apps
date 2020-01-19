63118259ed457c427d2e73f6544f6bee2aed1124
Georgimihalkov <Georgimihalkov@abv.bg>
2020-01-19 11:36:08 +0000

    Added a LogExceptionFilter to the ShopApp.Api project.

---
+++ ShopApp/ShopApp.Api/Infrastructure/Filters/LogRequestFIlter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Owin;
+using SimpleLogger;
+using SimpleLogger.Contracts;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace ShopApp.Api.Infrastructure.Filters
+{
+    public class LogRequestFIlter : ActionFilterAttribute
+    {
+        private readonly ILogManager logManager;
+
+        public LogRequestFIlter()
+        {
+            this.logManager = new LogManager(); 
+        }
+
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            var owinContext = actionContext.Request.Properties["MS_OwinContext"] as OwinContext;
+            
+            this.logManager.ConsoleLog($"IP: {owinContext.Request.RemoteIpAddress} Method: {actionContext.Request.Method} Host: {actionContext.Request.Headers.Host} Route: {actionContext.Request.RequestUri.PathAndQuery}");
+        }
+    }
+}
