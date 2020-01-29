19292f77490cf0b32a392b074842f21d32d1c7d1
Georgimihalkov <Georgimihalkov@abv.bg>
2020-01-29 21:10:57 +0000

    Added a roles role model and roles enum.

---
+++ ShopApp/ShopApp.Dal/Models/Roles/ShopRole.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopApp.Dal.Models.Roles
+{
+    public class ShopRole
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ ShopApp/ShopApp.Web/Constants/RolesEnum.cs
@@ -0,0 +1,8 @@
+﻿namespace ShopApp.Web.Constants
+{
+    public enum RolesEnum
+    {
+        User = 0,
+        Administrator = 1
+    }
+}
