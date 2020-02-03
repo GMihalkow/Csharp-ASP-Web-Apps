dbd9c4820041ca4a7bb8167d17a565e34879323a
Georgi <georgimihalkov@abv.bg>
2020-02-03 23:30:17 +0200

    Added the Login and Register models.

---
+++ ShopApp2.0/ShopApp.Web/Models/Account/LoginInputModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShopApp.Web.Models
+{
+	public class LoginInputModel
+	{
+		[Required]
+		[Display(Name = "Username")]
+		public string UserName { get; set; }
+
+		[Required]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+	}
+}
---
+++ ShopApp2.0/ShopApp.Web/Models/Account/RegisterInputModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShopApp.Web.Models
+{
+	public class RegisterInputModel
+	{
+		[Required]
+		[MinLength(6)]
+		[Display(Name = "Username")]
+		public string UserName { get; set; }
+
+		[MinLength(6)]
+		public string FirstName { get; set; }
+
+		[MinLength(6)]
+		public string LastName { get; set; }
+
+		[Required]
+		[DataType(DataType.Password)]
+		[Compare(nameof(ConfirmPassword))]
+		public string Password { get; set; }
+
+		[Required]
+		[Compare(nameof(Password))]
+		[DataType(DataType.Password)]
+		[Display(Name = "Confirm Password")]
+		public string ConfirmPassword { get; set; }
+
+		[Required]
+		[EmailAddress]
+		[Display(Name = "Email Address")]
+		public string Email { get; set; }
+
+        [DataType(DataType.Date)]
+		public DateTime? BirthDate { get; set; }
+
+        [Required]
+        [Display(Name = "Phone Number")]
+        [RegularExpression(@"^[0-9]{10}$", ErrorMessage = "PhoneNumber should contain exactly 10 numbers.")]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+    }
+}
