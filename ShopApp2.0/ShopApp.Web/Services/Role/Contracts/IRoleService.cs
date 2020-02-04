6619ede37c0b9d5f61251fb8b6ae736c0dce8c00
Georgi <georgimihalkov@abv.bg>
2020-02-04 21:54:56 +0200

    Extracted some of the Roles logic into a RoleServcie class.

--- ShopApp2.0/ShopApp.Web/Services/Role/Contracts/IRoleService.cs
+++ ShopApp2.0/ShopApp.Web/Services/Role/Contracts/IRoleService.cs
@@ -1,7 +1,9 @@
+using System.Threading.Tasks;
+
 namespace ShopApp.Web.Services.Role.Contracts
 {
     public interface IRoleService
     {
-        
+        Task SeedRoles();
     }
 }
--- ShopApp2.0/ShopApp.Web/Services/Role/RoleService.cs
+++ ShopApp2.0/ShopApp.Web/Services/Role/RoleService.cs
@@ -1,7 +1,32 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using ShopApp.Web.Constants;
+using ShopApp.Web.Services.Role.Contracts;
+
 namespace ShopApp.Web.Services.Role
 {
-    public class RoleService
+    public class RoleService : IRoleService
     {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RoleService(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
         
+        public async Task SeedRoles()
+        {
+            var userRoleExists = await this._roleManager.RoleExistsAsync(RolesConstants.User);
+            if (!userRoleExists)
+            {
+                await this._roleManager.CreateAsync(new IdentityRole {Name = RolesConstants.User});
+            }
+
+            var adminRoleExists = await this._roleManager.RoleExistsAsync(RolesConstants.Administrator);
+            if (!adminRoleExists)
+            {
+                await this._roleManager.CreateAsync(new IdentityRole {Name = RolesConstants.Administrator});
+            }
+        }
     }
 }
