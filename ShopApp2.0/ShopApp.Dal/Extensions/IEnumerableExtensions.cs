46cab473e636b4d290d7448e61de67715ac8bdd8
Georgi <georgimihalkov@abv.bg>
2020-02-06 23:38:21 +0200

    Added Dal Extensions.

---
+++ ShopApp2.0/ShopApp.Dal/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ShopApp.Dal
+{
+    public static class IEnumerableExtensions
+    {
+
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> collection)
+        {
+            return collection == null || collection?.Count() == 0;
+        }
+
+        public static IEnumerable<T> OrderBy<T>(this IEnumerable<T> collection, string property)
+            where T : new()
+        {
+            return collection.OrderBy(x => x.GetType().GetProperty(property)?.GetValue(x, null));
+        }
+
+        public static IEnumerable<T> OrderByDescending<T>(this IEnumerable<T> collection, string property)
+            where T : new()
+        {
+            return collection.OrderByDescending(x => x.GetType().GetProperty(property)?.GetValue(x, null));
+        }
+    }
+}
