a9262ad486b7932c08706b5bac31c30f67af1840
Georgi <georgimihalkov@abv.bg>
2020-02-06 23:38:07 +0200

    Added Dal Constants.

---
+++ ShopApp2.0/ShopApp.Dal/Constants/DalConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopApp.Dal
+{
+    public class DalConstants
+    {
+        public const int PageSize = 9;
+
+        public const string UrlRegex = @"^(https|http)?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/\/=]*)$";
+    }
+}
