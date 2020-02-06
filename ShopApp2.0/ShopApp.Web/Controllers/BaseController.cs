babf02f5a34d6dc8e3c302cfbb40aeb7721c6314
Georgi <georgimihalkov@abv.bg>
2020-02-06 23:40:38 +0200

    Added the LoggedUserId property to the BaseController and made it an abstract class.

--- ShopApp2.0/ShopApp.Web/Controllers/BaseController.cs
+++ ShopApp2.0/ShopApp.Web/Controllers/BaseController.cs
@@ -1,9 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
+using ShopApp.Dal.Services.User.Contracts;
 
 namespace ShopApp.Web.Controllers
 {
-    public class BaseController : Controller
+    public abstract class BaseController : Controller
     {
-        
+        protected string LoggedUserId
+        {
+            get
+            {
+                if (this.User.Identity.IsAuthenticated)
+                {
+                    var userService = this.HttpContext.RequestServices.GetService(typeof(IUserService)) as IUserService;
+
+                    var currentUserId = userService.GetUserByName(this.User.Identity.Name)?.Id;
+
+                    return currentUserId;
+                }
+                
+                return default(string);
+            }
+        }
     }
 }
