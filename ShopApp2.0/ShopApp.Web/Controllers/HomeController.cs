1baaee0b26b029048d2d88bc7e24acae048786db
Georgi <georgimihalkov@abv.bg>
2020-02-23 13:53:11 +0200

    Begun implementing the index page layout.

--- ShopApp2.0/ShopApp.Web/Controllers/HomeController.cs
+++ ShopApp2.0/ShopApp.Web/Controllers/HomeController.cs
@@ -1,12 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
+using ShopApp.Dal;
+using ShopApp.Dal.Repositories.Contracts;
 
 namespace ShopApp.Web.Controllers
 {
     public class HomeController : BaseController
     {
+        private readonly IRepository<ProductViewModel, ProductBaseInputModel> _productRepository;
+
+        public HomeController(IRepository<ProductViewModel, ProductBaseInputModel> productRepository)
+        {
+            _productRepository = productRepository;
+        }
+    
         public IActionResult Index()
         {
-            return this.View();
+            var products = this._productRepository.GetAll();
+            
+            return this.View(products);
         }
     }
 }
